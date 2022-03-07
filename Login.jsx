58efd04615a8975496efb808d5c387da939a42ff
Deva <you@example.comdevabalagurusamy@gmail.com>
2022-03-07 10:11:38 +0530

    front end pages

--- Login.jsx
+++ Login.jsx
@@ -1,40 +1,44 @@
 import React from 'react'
-import './login.css';
+import './LogIn.css';
+import '../../App.css';
 
 export default function Login() {
     return (
-        
-        <div className='loginForm'>
-            
-            <div className="loginHeader">
-                <h3 className='loginArea'>Login</h3>
-            </div>
-        
-            <div className="boxingLogin">
-            <div className='userCredentialsArea'>
-                <form className='loginUser'>
-                    <div className='userLoginCredential' id='email'>
-                    <input type='email' placeholder='Enter email' className='inputs'></input>
+        <div className="log-in">
+            <div className='loginForm'>
 
-                    </div>
-                    <div className='userLoginCredential' id='password' >
-                    <input type='password' placeholder='Enter password' className='inputs'></input>
+                <div className="loginHeader">
+                   
+                </div>
 
-                    </div>
-                    <div className="buttonForLogin">
+                <div className="boxingLogin">
                     
-                    <button className="loginButton">Login</button>
-                   
-                    </div>
-                   
-                  
+                    <div className='userCredentialsArea'>
                         
-                
-                </form>
-            </div>
+                    <h3 className='loginArea'>Login</h3>
+                        <form className='loginUser'>
+                            <div className='userLoginCredential' id='email'>
+                                <input type='email' placeholder='Enter email' className='inputArea'></input>
+
+                            </div>
+                            <div className='userLoginCredential' id='password' >
+                                <input type='password' placeholder='Enter password' className='inputArea'></input>
+
+                            </div>
+                            <div className="buttonForLogin">
+
+                                <button className="loginButton">Login</button>
+
+                            </div>
+
+
+
+
+                        </form>
+                    </div>
+                </div>
             </div>
         </div>
-       
-       
+
     )
 }
